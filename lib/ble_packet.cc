57074a7b461925b2bb15adbb8c159f40e5a51cfc
Harry Hornbacher <harry@harry-hornbacher.de>
2017-09-06 15:47:09 +0200

    moved swapbits and dewhitening to BLEPacket

--- lib/ble_packet.cc
+++ lib/ble_packet.cc
@@ -7,6 +7,7 @@
 #include "ble_packet.h"
 
 #include <string.h>
+#include <stdio.h>
 
 namespace gr
 {
@@ -16,15 +17,51 @@ namespace ble
 /*
        * The private constructor
        */
-BLEPacket::BLEPacket(uint16_t header)
+BLEPacket::BLEPacket(uint8_t *header)
 {
-    memcpy(pduHeader, (uint8_t *)&header, 2);
+    memcpy(pduHeader, header, 2);
+    pduData = new uint8_t[getPDUDataLength()];
 }
 
 /*
        * Our virtual destructor.
        */
 BLEPacket::~BLEPacket()
+{
+    delete pduData;
+}
+
+//Taken from: https://stackoverflow.com/a/2602885
+uint8_t BLEPacket::swapbits(uint8_t b)
+{
+    b = (b & 0xF0) >> 4 | (b & 0x0F) << 4;
+    b = (b & 0xCC) >> 2 | (b & 0x33) << 2;
+    b = (b & 0xAA) >> 1 | (b & 0x55) << 1;
+    return b;
+}
+
+void BLEPacket::dewhitening(uint8_t channel, uint8_t *data, int length, uint8_t *out)
+{
+    uint16_t lfsr = swapbits(channel) | 2;
+
+    for (int i = 0; i < length; i++)
+    {
+        uint8_t d = swapbits(data[i]);
+        for (int bit = 0; bit < 8; bit++)
+        {
+            uint8_t b = 1 << bit;
+            if (lfsr & 0x80)
+            {
+                lfsr ^= 0x11;
+                d ^= b;
+            }
+            lfsr = lfsr << 1;
+        }
+        out[i] = d;
+    }
+}
+
+bool BLEPacket::crc()
 {
 }
 
@@ -33,9 +70,56 @@ PDUType BLEPacket::getPDUType()
     return (PDUType)(pduHeader[0] & 0x0f);
 }
 
+void BLEPacket::getPDUTypeName(char *dst, int max)
+{
+    PDUType pduType = getPDUType();
+    if (pduType == ADV_IND)
+        snprintf(dst, max, "ADV_IND");
+    else if (pduType == ADV_DIRECT_IND)
+        snprintf(dst, max, "ADV_IND");
+    else if (pduType == SCAN_REQ)
+        snprintf(dst, max, "ADV_IND");
+    else if (pduType == SCAN_RSP)
+        snprintf(dst, max, "ADV_IND");
+    else if (pduType == CONNECT_REQ)
+        snprintf(dst, max, "ADV_IND");
+    else if (pduType == ADV_SCAN_IND)
+        snprintf(dst, max, "ADV_IND");
+    else if (pduType == ADV_NONCONN_IND)
+        snprintf(dst, max, "ADV_IND");
+    else
+        snprintf(dst, max, "UNKNOWN: 0x%02x", pduType);
+}
+
 uint8_t BLEPacket::getPDUDataLength()
 {
     return pduHeader[1];
+}
+
+bool BLEPacket::setPDUData(boost::circular_buffer<uint8_t> &rbuff)
+{
+    if (rbuff.size() < getPDUDataLength())
+        return false;
+    for (int i = 0; i < getPDUDataLength(); i++)
+    {
+        pduData[i] = rbuff.front();
+        rbuff.pop_front();
+    }
+    return true;
+}
+
+bool BLEPacket::isPDUValid()
+{
+    PDUType pduType = getPDUType();
+    if (pduType == ADV_IND || pduType == ADV_DIRECT_IND || pduType == SCAN_REQ || pduType == SCAN_RSP || pduType == CONNECT_REQ || pduType == ADV_SCAN_IND || pduType == ADV_NONCONN_IND)
+    {
+        return true;
+    }
+    return false;
+}
+
+bool BLEPacket::isCRCValid()
+{
 }
 
 } /* namespace ble */
--- lib/ble_packet.h
+++ lib/ble_packet.h
@@ -4,6 +4,7 @@
 #define INCLUDED_BLE_BLEPACKET_H
 
 #include <stdint.h>
+#include <boost/circular_buffer.hpp>
 
 namespace gr
 {
@@ -12,31 +13,38 @@ namespace ble
 
 enum PDUType
 {
-    ADV_IND = 0b0000,
-    ADV_DIRECT_IND = 0b0001,
-    ADV_NONCONN_IND = 0b0010,
-    SCAN_REQ = 0b0011,
-    SCAN_RSP = 0b0100,
-    CONNECT_REQ = 0b0101,
-    ADV_SCAN_IND = 0b0110
+  ADV_IND = 0b0000,
+  ADV_DIRECT_IND = 0b0001,
+  ADV_NONCONN_IND = 0b0010,
+  SCAN_REQ = 0b0011,
+  SCAN_RSP = 0b0100,
+  CONNECT_REQ = 0b0101,
+  ADV_SCAN_IND = 0b0110
 };
 
 class BLEPacket
 {
-  private:
-    uint8_t pduHeader[2];
-    uint8_t *pduData;
-    uint32_t crc24;
+private:
+  uint8_t pduHeader[2];
+  uint8_t *pduData;
+  uint32_t crc24;
 
-  public:
-    BLEPacket(uint16_t pduHeader);
-    ~BLEPacket();
+  uint8_t swapbits(uint8_t b);
+  void dewhitening(uint8_t channel, uint8_t *data, int length, uint8_t *out);
+  bool crc();
 
-    PDUType getPDUType();
-    uint8_t getPDUDataLength();
-    bool isValid();
+public:
+  BLEPacket(uint8_t *pduHeader);
+  ~BLEPacket();
 
-    void setPDUData();
+  PDUType getPDUType();
+  void getPDUTypeName(char *dst, int max);
+  uint8_t getPDUDataLength();
+
+  bool setPDUData(boost::circular_buffer<uint8_t> &rbuff);
+
+  bool isPDUValid();
+  bool isCRCValid();
 };
 
 } // namespace ble
